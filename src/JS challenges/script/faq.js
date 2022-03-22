8df21cd85f6a2e99879cd93de0114ab2e1ee3c5d
Keshavaraj M <kesh3008@global.chainsys.com>
2022-03-22 13:30:53 +0530

    rk commit

--- src/JS challenges/script/faq.js
+++ src/JS challenges/script/faq.js
@@ -1,62 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let containerStyle = window.getComputedStyle(
-    document.querySelector(".container")
-  );
-  let boxImage = document.querySelector(".top-image");
-  let boxImageStyle = window.getComputedStyle(boxImage);
+const buttonOpen = document.querySelectorAll("#button");
+const answer = document.querySelectorAll(".answer");
+const imgArrow = document.querySelectorAll(".img");
+const boldText = document.querySelectorAll(".boldText");
 
-  ChangeBoxPosition();
+let x;
+function openClose(place) {
+  if (x == 1) {
+    answer[place].classList.add("answer");
+    imgArrow[place].classList.remove("imgArrow");
+    boldText[place].classList.remove("bold");
+    return (x = 0);
+  } else {
+    answer[place].classList.remove("answer");
+    imgArrow[place].classList.add("imgArrow");
+    boldText[place].classList.add("bold");
+    return (x = 1);
+  }
+}
 
-  window.addEventListener("resize", ChangeBoxPosition);
-
-  document.querySelectorAll("dt").forEach((item) => {
-    item.addEventListener("click", UserClick);
+for (let i = 0; i < buttonOpen.length; i++) {
+  buttonOpen[i].addEventListener("click", function () {
+    openClose(i);
   });
-
-  function UserClick(e) {
-    if (e.target.className == "list__word") {
-      let selectedQuestion = document.querySelector(".list__word--selected");
-
-      if (selectedQuestion != null) {
-        DisableQuestion(selectedQuestion);
-      }
-
-      EnableQuestion(e.target);
-    } else {
-      DisableQuestion(e.target);
-    }
-  }
-
-  function DisableQuestion(question) {
-    question.classList.remove("list__word--selected");
-    question.children[1].classList.remove("flip-vertically");
-    question.nextElementSibling.classList.remove("list__description--selected");
-  }
-
-  function EnableQuestion(question) {
-    question.classList.add("list__word--selected");
-    question.children[1].classList.add("flip-vertically");
-    question.nextElementSibling.classList.add("list__description--selected");
-  }
-
-  function ChangeBoxPosition() {
-    if (document.body.clientWidth < 768) {
-      boxImage.style.left =
-        document.body.clientWidth / 2 -
-        parseFloat(boxImageStyle.width) / 2 +
-        "px";
-      boxImage.style.top = "50px";
-    } else {
-      boxImage.style.left =
-        parseFloat(containerStyle.marginLeft) -
-        parseFloat(boxImageStyle.width) / 2 +
-        10 +
-        "px";
-      boxImage.style.top =
-        parseFloat(containerStyle.marginTop) +
-        parseFloat(boxImageStyle.height) +
-        10 +
-        "px";
-    }
-  }
-});
+}
